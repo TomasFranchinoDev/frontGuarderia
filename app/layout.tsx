b8f37a0885fb1ad0282419e8e1c57d30777506fe
TomasFranchino <tomi.franc2@gmail.com>
2026-01-23 00:05:34 -0300

    Agrego Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 // 1. AGREGA 'display: "swap"' A LAS FUENTES
 const geistSans = Geist({
@@ -41,6 +42,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
