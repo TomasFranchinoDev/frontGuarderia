1335a6dead79271bbd53b6ed00053713c12e5ceb
TomasFranchino <tomi.franc2@gmail.com>
2026-01-23 00:19:41 -0300

    Agrego Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next"
 
 // 1. AGREGA 'display: "swap"' A LAS FUENTES
 const geistSans = Geist({
@@ -43,6 +44,7 @@ export default function RootLayout({
       >
         {children}
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
